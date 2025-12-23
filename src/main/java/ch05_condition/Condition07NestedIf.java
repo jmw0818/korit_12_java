3eaaf96c1d382c84d8c36ce036791f945792a96d
jmw0818 <wjdauddnjs12345@gamil.com>
2025-12-23 15:56:18 +0900

    gd

--- src/main/java/ch05_condition/Condition07NestedIf.java
+++ src/main/java/ch05_condition/Condition07NestedIf.java
@@ -1,7 +1,64 @@
 package ch05_condition;
+/*
+    5. 중첩 if문(Nested - if sentence) :
+        if 문 내에 if 문이 중첩적으로 사용될 수 있습니다.
+
+    형식 :
+    if (조건식 1) {
+        실행문 1-1;
+        if (조건식 1a) {
+            실행문 1a;
+        } else if (조건식 1b) {
+            실행문 1b;
+        } else if (조건식 1c) {
+            실행문 1c;
+        } else {
+            실행문 1d;
+        }
+        실행문 1-2;
+    } (조건식 2) {
+        실행문 2-1;
+        if (조건식 2a) {
+            실행문 2a;
+        } else if (조건식 2b) {
+            실행문 2b;
+        } else if (조건식 2c) {
+            실행문 2c;
+        } else {
+            실행문 2d;
+        }
+        실행문 2-2;
+    } else {
+        실행문 3;
+    }
+ */
+
+import java.util.Scanner;
 
 public class Condition07NestedIf {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int score = 0;
+        String grade = "";
+        System.out.print("점수를 입력하세요 >>> ");
+        score = scanner.nextInt();
 
+        if (score > 100 || score < 0) {
+            System.out.println("불가능한 점수 입력입니다.");
+            grade = "X";
+        } else {
+            if (score >= 90) {
+                grade = "A";
+            } else if (score >= 80) {
+                grade = "B";
+            } else if (score >= 70) {
+                grade = "C";
+            } else if (score >= 60) {
+                grade = "D";
+            } else {
+                grade = "F";
+            }
+        }
+        System.out.println("당신의 점수는 " + score + "점이고, 등급은 " + grade + "입니다.");
     }
 }
---
+++ src/main/java/ch05_condition/Condition10.java
@@ -0,0 +1,47 @@
+package ch05_condition;
+/*
+    삼항 연산자 : 조건식을 평가하여 true / false에 따라 두 가지 표현식 중 하나를 선택적으로 실행
+
+    형식 :
+    조건식 ? 표현식 1 : 표현식 2
+
+    조건식 : true / false로 평가 가능한 식
+    표현식 1 : 조건식이 true일 때 실행되는 식
+    표현식 2 : 조건식이 false일 때 실행되는 식
+ */
+
+import java.util.Scanner;
+
+public class Condition10 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        int max;
+        max = a > b ? a : b;
+        System.out.println("max에 저장된 값 : " + max);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("연도를 입력하세요 >>> ");
+        int year = scanner.nextInt();
+//        boolean isLeapYear = ( year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+//        String result = isLeapYear ? "윤년입니다." : "윤년이 아닙니다.";
+//        System.out.println(year + "년은 " + result);
+
+        String result2 = "";
+        boolean isLeapYear2 = ( year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+        if (year < 0) {
+            result2 = "불가능한 연도 입력입니다.";
+        } else {
+            result2 = isLeapYear2 ? "윤년입니다." : "윤년이 아닙니다.";
+        }
+        System.out.println(year + "년은 " + result2);
+
+        String result3 = year < 0 ? "불가능한 연도 입력입니다." :
+            ( year % 4 == 0 && year % 100 != 0) || (year % 400 == 0) ?
+                        "윤년입니다." : "윤년이 아닙니다.";
+        System.out.println(year + "년은 " + result3);
+
+
+
+    }
+}
